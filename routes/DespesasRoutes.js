228580ddd80c5204c4a957b41d3bba65064c8a0d
Márcio Ferreira <hmarciogferreira@gmail.com>
2024-03-09 12:03:52 -0300

    ajustes

--- routes/DespesasRoutes.js
+++ routes/DespesasRoutes.js
@@ -4,10 +4,10 @@ import DespesasController from '../controllers/DespesasController.js';
 const DespesasRoutes = express.Router();
 
 const despesasController = new DespesasController();
-DespesasRoutes.get('/usuarios', despesasController.findAll);
-DespesasRoutes.get('/usuarios/:id', despesasController.findOne);
-DespesasRoutes.post('/usuarios', despesasController.create);
-DespesasRoutes.put('/usuarios/:id', despesasController.update);
-DespesasRoutes.delete('/usuarios/:id', despesasController.delete);
+DespesasRoutes.get('/despesas', despesasController.findAll);
+DespesasRoutes.get('/despesas/:id', despesasController.findOne);
+DespesasRoutes.post('/despesas', despesasController.create);
+DespesasRoutes.put('/despesas/:id', despesasController.update);
+DespesasRoutes.delete('/despesas/:id', despesasController.delete);
 
 export default DespesasRoutes;
